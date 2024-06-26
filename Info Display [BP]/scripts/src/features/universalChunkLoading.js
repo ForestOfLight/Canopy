32d8b11a8f0269022cb4a92851359d084e4edf1a
ForestOfLight <briancassriel@gmail.com>
2024-06-26 10:58:39 +0200

    univeralChunkLoading bugfix

--- Info Display [BP]/scripts/src/features/universalChunkLoading.js
+++ Info Display [BP]/scripts/src/features/universalChunkLoading.js
@@ -2,9 +2,5 @@ import * as mc from '@minecraft/server'
 
 mc.world.afterEvents.entitySpawn.subscribe((event) => {
     if (event.entity.typeId !== 'minecraft:minecart' || !mc.world.getDynamicProperty('universalChunkLoading')) return;
-    const entity = event.entity;
-    entity.triggerEvent('info:enable_ticking');
-    mc.system.runTimeout(() => {
-        if (entity.isValid()) entity.triggerEvent('info:disable_ticking');
-    }, 200);
+    event.entity.triggerEvent('info:tick_tenSeconds');
 });
