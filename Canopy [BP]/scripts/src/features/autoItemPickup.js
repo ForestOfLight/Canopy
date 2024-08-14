5033c1aa287b9976d70dc2ae9d0413067230d5c5
ForestOfLight <briancassriel@gmail.com>
2024-08-14 13:45:20 -0700

    remove faulty  autoItemPickup check

--- Canopy [BP]/scripts/src/features/autoItemPickup.js
+++ Canopy [BP]/scripts/src/features/autoItemPickup.js
@@ -9,7 +9,6 @@ system.runInterval(() => {
 
 world.afterEvents.playerBreakBlock.subscribe(blockEvent => {
     if (!world.getDynamicProperty('autoItemPickup')) return;
-    if (!blockEvent.player.getDynamicProperty('autoItemPickup')) return;
     if (blockEvent.player.getGameMode() === 'creative') return;
     brokenBlockEventsThisTick.push(blockEvent);
 });
