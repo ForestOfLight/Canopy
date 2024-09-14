fa251f732a5360e53edd2ff57533a40b62368408
ForestOfLight <briancassriel@gmail.com>
2024-09-14 12:43:05 -0700

    create commandGamemode rule to add permission wall to shortened gamemode commands

--- Canopy [BP]/scripts/src/commands/gamemode.js
+++ Canopy [BP]/scripts/src/commands/gamemode.js
@@ -1,24 +1,33 @@
-import { Command } from 'lib/canopy/Canopy';
+import { Rule, Command } from 'lib/canopy/Canopy';
+
+new Rule({
+    category: 'Rules',
+    identifier: 'commandGamemode',
+    description: 'Enables shortened gamemode commands (e.g. `./s` for survival), including without OP.',
+})
 
 new Command({
     name: 's',
     description: 'Set your gamemode to survival.',
     usage: 's',
-    callback: (sender) => setPlayerGamemode(sender, 'survival')
+    callback: (sender) => setPlayerGamemode(sender, 'survival'),
+    contingentRules: ['commandGamemode']
 });
 
 new Command({
     name: 'c',
     description: 'Set your gamemode to creative.',
     usage: 'c',
-    callback: (sender) => setPlayerGamemode(sender, 'creative')
+    callback: (sender) => setPlayerGamemode(sender, 'creative'),
+    contingentRules: ['commandGamemode']
 });
 
 new Command({
     name: 'sp',
     description: 'Set your gamemode to spectator.',
     usage: 'sp',
-    callback: (sender) => setPlayerGamemode(sender, 'spectator')
+    callback: (sender) => setPlayerGamemode(sender, 'spectator'),
+    contingentRules: ['commandGamemode']
 });
 
 function setPlayerGamemode(sender, gamemode) {
