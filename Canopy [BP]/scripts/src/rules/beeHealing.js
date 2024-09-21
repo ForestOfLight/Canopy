b96816d222037aaec5fd49a5aa2c275a5243bb60
ForestOfLight <briancassriel@gmail.com>
2024-09-21 01:38:08 -0700

    add no entity check

--- Canopy [BP]/scripts/src/rules/beeHealing.js
+++ Canopy [BP]/scripts/src/rules/beeHealing.js
@@ -8,7 +8,7 @@ new Rule({
 });
 
 world.afterEvents.entityHurt.subscribe(event => {
-    if (event.hurtEntity.typeId !== 'minecraft:bee' || event.damageSource.cause !== 'drowning') return;
+    if (event.hurtEntity?.typeId !== 'minecraft:bee' || event.damageSource.cause !== 'drowning') return;
     const healthComponent = event.hurtEntity.getComponent('minecraft:health')
     healthComponent.setCurrentValue(healthComponent.currentValue + event.damage);
 });
