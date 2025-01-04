64e1b4063ffb2353dba8aba6864d006d1469d6a7
ForestOfLight <briancassriel@gmail.com>
2025-01-04 06:28:43 -0800

    changedimension -> dtp and coord conversion improvements

--- Canopy [BP]/scripts/src/commands/changedimension.js
+++ Canopy [BP]/scripts/src/commands/changedimension.js
@@ -5,11 +5,14 @@ import { world } from '@minecraft/server';
 const validDimensions = {
     'o': 'overworld',
     'overworld': 'overworld',
+    'minecraft:overworld': 'overworld',
     'n': 'nether',
     'nether': 'nether',
+    'minecraft:the_nether': 'nether',
     'e': 'the_end',
     'end': 'the_end',
     'the_end': 'the_end',
+    'minecraft:the_end': 'the_end'
 };
 
 new Rule({
@@ -19,9 +22,9 @@ new Rule({
 });
 
 const cmd = new Command({
-    name: 'changedimension',
+    name: 'dtp',
     description: { translate: 'commands.changedimension' },
-    usage: 'changedimension <dimension> [x y z]',
+    usage: 'dtp <dimension> [x y z]',
     args: [
         { type: 'string', name: 'dimension' },
         { type: 'number', name: 'x' },
@@ -36,19 +39,29 @@ function changedimensionCommand(player, args) {
     const { dimension, x, y, z } = args;
     if (!dimension)
         return cmd.sendUsage(player);
-    const validDimensionId = validDimensions[dimension.toLowerCase()];
-    if (!validDimensionId)
+    const toDimensionId = validDimensions[dimension.toLowerCase()];
+    if (!toDimensionId)
         return player.sendMessage({ translate: 'commands.changedimension.notfound', with: [Object.keys(validDimensions).join(', ')] });
     
-    const validDimension = world.getDimension(validDimensionId);
+    const fromDimensionId = player.dimension.id.replace('minecraft:', '');
+    const toDimension = world.getDimension(toDimensionId);
     if ((x !== null && y !== null && z !== null) && (Utils.isNumeric(x) && Utils.isNumeric(y) && Utils.isNumeric(z))) {
         const location = { x, y, z };
-        player.teleport(location, { dimension: validDimension } );
-        player.sendMessage({ translate: 'commands.changedimension.success.coords', with: [Utils.stringifyLocation(location), validDimensionId] });
+        player.teleport(location, { dimension: toDimension } );
+        player.sendMessage({ translate: 'commands.changedimension.success.coords', with: [Utils.stringifyLocation(location), toDimensionId] });
     } else if (x === null && y === null && z === null) {
-        player.teleport(player.location, { dimension: validDimension });
-        player.sendMessage({ translate: 'commands.changedimension.success', with: [validDimensionId] });
+        player.teleport(convertCoords(fromDimensionId, toDimensionId, player.location), { dimension: toDimension });
+        player.sendMessage({ translate: 'commands.changedimension.success', with: [toDimensionId] });
     } else {
         player.sendMessage({ translate: 'commands.changedimension.fail.coords' });
     }
+}
+
+function convertCoords(fromDimension, toDimension, location) {
+    if (fromDimension === 'overworld' && toDimension === 'nether')
+        return { x: location.x / 8, y: location.y, z: location.z / 8 };
+    else if (fromDimension === 'nether' && toDimension === 'overworld')
+        return { x: location.x * 8, y: location.y, z: location.z * 8 };
+    else
+        return location;
 }
