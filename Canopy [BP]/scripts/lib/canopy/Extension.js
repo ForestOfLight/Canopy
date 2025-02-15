91257e82ad057d5c4d4a718312af2ccbdef9a90b
ForestOfLight <briancassriel@gmail.com>
2025-02-14 18:00:32 -0800

    remove some logging

--- Canopy [BP]/scripts/lib/canopy/Extension.js
+++ Canopy [BP]/scripts/lib/canopy/Extension.js
@@ -70,14 +70,12 @@ class Extension {
 
     #setupCommandRegistration() {
         IPC.on(`canopyExtension:${this.id}:registerCommand`, (cmdData) => {
-            console.warn(`[Canopy] Registering command: ${cmdData.name}`);
             this.commands.push(new Command(cmdData));
         });
     }
 
     #setupRuleRegistration() {
         IPC.on(`canopyExtension:${this.id}:registerRule`, (ruleData) => {
-            console.warn(`[Canopy] Registering rule: ${ruleData.identifier}`);
             this.rules.push(new Rule({ category: "Rules", ...ruleData }));
         });
     }
