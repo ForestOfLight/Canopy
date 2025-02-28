f12cebd29d468b5b65616ae54c1c95aed3a9fa2e
ForestOfLight <briancassriel@gmail.com>
2025-02-27 18:19:45 -0800

    fix replaceAll potential miss

--- Canopy [BP]/scripts/include/utils.js
+++ Canopy [BP]/scripts/include/utils.js
@@ -243,7 +243,7 @@ export function titleCase(str) {
 	return str
 		.replace(/([a-z])([A-Z])/g, '$1 $2')
 		.toLowerCase()
-		.replaceAll('_', ' ')
+		.replace(/_/g, ' ')
 		.split(' ')
 		.map(word => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(' ');
