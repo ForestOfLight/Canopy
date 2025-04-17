de108d0524701b9ccf0b5cd140279a1847261cf9
ForestOfLight <briancassriel@gmail.com>
2025-04-17 11:07:49 -0700

    Instaminable refactor and bugfix for refresh rate

--- Canopy [BP]/scripts/src/classes/Instaminable.js
+++ Canopy [BP]/scripts/src/classes/Instaminable.js
@@ -4,40 +4,46 @@ import { system, world } from "@minecraft/server";
 const beaconRefreshOffset = {};
 const BEACON_REFRESH_RATE = 80;
 
-world.afterEvents.effectAdd.subscribe(event => {
-    if (event.effect?.typeId !== 'haste' || event.entity?.typeId !== 'minecraft:player') return;
-    beaconRefreshOffset[event.entity.id] = system.currentTick % BEACON_REFRESH_RATE;
-});
-
 class Instaminable {
     constructor(litmusCallback, ruleId) {
         this.litmusCallback = litmusCallback;
         this.ruleId = ruleId;
-        this.init();
+        this.initEvents();
     }
 
-    init() {
-        system.runInterval(() => {
-            for (const player of world.getPlayers()) {
-                if (!player)
-                    continue;
-                if (player.getEffect('haste')?.amplifier === 2 && this.isTickBeforeRefresh(player)) 
-                    player.removeEffect('haste');
-                
-            }
-        });
+    initEvents() {
+        system.runInterval(this.onTick.bind(this), 1);
+        world.beforeEvents.playerBreakBlock.subscribe((event) => this.onPlayerBreakBlock(event));
+        world.afterEvents.effectAdd.subscribe((event) => this.onEffectAdd(event));
+    }
 
-        world.beforeEvents.playerBreakBlock.subscribe((event) => {
-            const blockId = event.block.typeId;
-            if (Rules.getNativeValue(this.ruleId) !== true) return;
-            if (!this.litmusCallback(blockId)) return;
-            const player = event.player;
-            if (this.isEfficiencyFiveNetheritePick(event.itemStack) && this.hasHasteTwo(player)) {
-                const duration = player.getEffect('haste')?.duration;
-                if (duration > 0)
-                    system.run(() => player.addEffect('haste', duration, { amplifier: 2 }));
-            }
-        });
+    onTick() {
+        for (const player of world.getPlayers()) {
+            if (!player)
+                continue;
+            if (player.getEffect('haste')?.amplifier === 2 && this.isTickBeforeRefresh(player))
+                player.removeEffect('haste');
+        }
+    }
+
+    onPlayerBreakBlock(event) {
+        const blockId = event.block.typeId;
+        if (Rules.getNativeValue(this.ruleId) !== true)
+            return;
+        if (!this.litmusCallback(blockId))
+            return;
+        const player = event.player;
+        if (this.isEfficiencyFiveNetheritePick(event.itemStack) && this.hasHasteTwo(player)) {
+            const duration = player.getEffect('haste')?.duration;
+            if (duration > 0)
+                system.run(() => player.addEffect('haste', duration, { amplifier: 2 }));
+        }
+    }
+
+    onEffectAdd(event) {
+        if (event.effect?.typeId !== 'minecraft:haste' || event.entity?.typeId !== 'minecraft:player')
+            return;
+        beaconRefreshOffset[event.entity.id] = system.currentTick % BEACON_REFRESH_RATE;
     }
 
     isTickBeforeRefresh(player) {
