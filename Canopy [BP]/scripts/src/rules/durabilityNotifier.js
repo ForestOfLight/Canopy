f89b350b075323e9852e34e35c4b30016e46cc4e
ForestOfLight <briancassriel@gmail.com>
2025-05-29 13:21:01 -0700

    fix durabilityNotifier?

--- Canopy [BP]/scripts/src/rules/durabilityNotifier.js
+++ Canopy [BP]/scripts/src/rules/durabilityNotifier.js
@@ -1,5 +1,5 @@
 import { Rule, Rules} from "../../lib/canopy/Canopy";
-import { world, GameMode } from '@minecraft/server';
+import { world, GameMode, ItemComponentTypes } from '@minecraft/server';
 
 const ACTIVE_DURABILITY = 2;
 const ADDITIONAL_DURABILITIES = [20];
@@ -10,17 +10,17 @@ const rule = new Rule({
     description: { translate: 'rules.durabilityNotifier', with: [ACTIVE_DURABILITY.toString()] }
 });
 
-world.afterEvents.playerBreakBlock.subscribe((event) => durabilityClink(event.player, event.itemStackBeforeBreak, event.itemStackAfterBreak));
-world.afterEvents.playerInteractWithBlock.subscribe((event) => durabilityClink(event.player, event.beforeItemStack, event.itemStack));
-world.afterEvents.playerInteractWithEntity.subscribe((event) => durabilityClink(event.player, event.beforeItemStack, event.itemStack));
+world.afterEvents.playerBreakBlock.subscribe((event) => durabilityNotifier(event.player, event.itemStackBeforeBreak, event.itemStackAfterBreak));
+world.afterEvents.playerInteractWithBlock.subscribe((event) => durabilityNotifier(event.player, event.beforeItemStack, event.itemStack));
+world.afterEvents.playerInteractWithEntity.subscribe((event) => durabilityNotifier(event.player, event.beforeItemStack, event.itemStack));
 
-function durabilityClink(player, beforeItemStack, itemStack) {
+function durabilityNotifier(player, beforeItemStack, itemStack) {
     if (!Rules.getNativeValue(rule.getID()) || !player || !itemStack || !beforeItemStack
         || player.getGameMode() === GameMode.creative
         || !usedDurability(beforeItemStack, itemStack)
     ) return;
     const durability = getRemainingDurability(itemStack);
-    if (ADDITIONAL_DURABILITIES.includes(durability)) 
+    if (ADDITIONAL_DURABILITIES.includes(durability))
         showNotification(player, durability);
     
     if (durability <= ACTIVE_DURABILITY) {
@@ -30,12 +30,12 @@ function durabilityClink(player, beforeItemStack, itemStack) {
 }
 
 function usedDurability(beforeItemStack, afterItemStack) {
-    return afterItemStack.hasComponent('durability') && beforeItemStack.hasComponent('durability')
-        && (beforeItemStack.getComponent('durability').damage < afterItemStack.getComponent('durability').damage);
+    return afterItemStack.hasComponent(ItemComponentTypes.Durability) && beforeItemStack.hasComponent(ItemComponentTypes.Durability)
+        && (beforeItemStack.getComponent(ItemComponentTypes.Durability).damage < afterItemStack.getComponent(ItemComponentTypes.Durability).damage);
 }
 
 function getRemainingDurability(itemStack) {
-    const durabilityComponent = itemStack.getComponent('durability');
+    const durabilityComponent = itemStack.getComponent(ItemComponentTypes.Durability);
     if (!durabilityComponent) return undefined;
     return durabilityComponent.maxDurability - durabilityComponent.damage;
 }
