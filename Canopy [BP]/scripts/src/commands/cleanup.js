aa6d2a3e8a902f60136219c16888dae38d1253c4
ForestOfLight <briancassriel@gmail.com>
2025-07-03 18:08:26 -0700

    k transitioned to vanilla command

--- Canopy [BP]/scripts/src/commands/cleanup.js
+++ Canopy [BP]/scripts/src/commands/cleanup.js
@@ -5,32 +5,40 @@ system.beforeEvents.startup.subscribe((event) => {
     commandRegistry.registerCommand({
         name: 'canopy:cleanup',
         description: 'commands.cleanup',
-        optionalParameters: [{ name: 'distance', type: CustomCommandParamType.Integer }],
+        optionalParameters: [{ name: 'radius', type: CustomCommandParamType.Integer }],
+        permissionLevel: CommandPermissionLevel.GameDirectors,
+        cheatsRequired: true
+    }, cleanupCommand);
+
+    commandRegistry.registerCommand({
+        name: 'canopy:k',
+        description: 'commands.cleanup',
+        optionalParameters: [{ name: 'radius', type: CustomCommandParamType.Integer }],
         permissionLevel: CommandPermissionLevel.GameDirectors,
         cheatsRequired: true
     }, cleanupCommand);
 });
 
 const TRASH_ENTITY_TYPES = ['minecraft:item', 'minecraft:xp_orb'];
-const DEFAULT_DISTANCE = 50;
+const DEFAULT_RADIUS = 50;
 
-function cleanupCommand(origin, distance) {
-    if (!distance)
-        distance = DEFAULT_DISTANCE;
+function cleanupCommand(origin, radius) {
+    if (!radius)
+        radius = DEFAULT_RADIUS;
     const source = origin.sourceBlock || origin.sourceEntity;
     if (!source)
         return { status: CustomCommandStatus.Failure, message: 'generic.source.notfound' };
-    const removedCount = removeTrashEntities(source, distance);
+    const removedCount = removeTrashEntities(source, radius);
     if (origin.sourceBlock)
         return { status: CustomCommandStatus.Success, message: 'commands.cleanup.success' };
-    source.sendMessage({ translate: 'commands.cleanup.success', with: [removedCount.toString(), distance.toString()] });
+    source.sendMessage({ translate: 'commands.cleanup.success', with: [removedCount.toString(), radius.toString()] });
     return { status: CustomCommandStatus.Success };
 }
 
-function removeTrashEntities(source, distance) {
+function removeTrashEntities(source, radius) {
     let removedCount = 0;
     for (const type of TRASH_ENTITY_TYPES) {
-        const entities = source.dimension.getEntities({ type: type, location: source.location, maxDistance: distance });
+        const entities = source.dimension.getEntities({ type: type, location: source.location, maxDistance: radius });
         removedCount += entities.length;
         system.run(() => {
             for (const entity of entities)
