3a883754db9f118bcc06bf84774c9365d0dc2af6
ForestOfLight <briancassriel@gmail.com>
2025-11-10 20:45:22 -0800

    remove skip of first chunkBorders activation now that the crash is resolved

--- Canopy [BP]/scripts/src/rules/chunkBorders.js
+++ Canopy [BP]/scripts/src/rules/chunkBorders.js
@@ -1,22 +1,18 @@
-import { system, world } from "@minecraft/server";
 import { AbilityRule } from "../../lib/canopy/Canopy";
 import { ChunkBorderRender } from "../classes/ChunkBorderRender";
 import { playerChangeSubChunkEvent } from "../events/PlayerChangeSubChunkEvent";
 
 export class ChunkBorders extends AbilityRule {
     playerChunkBorderRenderers = {};
-    playersSkipNextEnable = {};
 
     constructor() {
         super({
             identifier: 'chunkBorders',
             onEnableCallback: () => {
                 playerChangeSubChunkEvent.subscribe(this.onPlayerChangeSubChunkBound);
-                world.afterEvents.playerJoin.subscribe(this.onPlayerJoin.bind(this));
             },
             onDisableCallback: () => {
                 playerChangeSubChunkEvent.unsubscribe(this.onPlayerChangeSubChunkBound);
-                world.afterEvents.playerJoin.unsubscribe(this.onPlayerJoin.bind(this));
                 this.stopAllRenderingBorders();
             }
         }, { 
@@ -26,7 +22,6 @@ export class ChunkBorders extends AbilityRule {
         });
         this.onPlayerChangeSubChunkBound = this.onPlayerChangeSubChunk.bind(this);
         this.chunkBorderRenderers = {};
-        this.playersSkipNextEnable = {};
     }
 
     stopAllRenderingBorders() {
@@ -50,18 +45,7 @@ export class ChunkBorders extends AbilityRule {
     drawChunkBorders(player) {
         if (this.chunkBorderRenderers[player.id])
             this.chunkBorderRenderers[player.id].destroy();
-        if (this.playersSkipNextEnable[player.id]) {
-            if (this.playersSkipNextEnable[player.id] === system.currentTick)
-                return;
-            delete this.playersSkipNextEnable[player.id];
-            return;
-        }
         this.chunkBorderRenderers[player.id] = new ChunkBorderRender(player.location);
-    }
-
-    onPlayerJoin(event) {
-        super.onPlayerJoin(event);
-        this.playersSkipNextEnable[event.playerId] = system.currentTick;
     }
 }
 
