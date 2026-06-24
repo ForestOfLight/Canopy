7fe4333af9f64d5f24c6b9ad9bb74d267f64f52e
ForestOfLight <briancassriel@gmail.com>
2026-06-23 17:06:38 -0700

    feat: add simplayer inventory and player info saver classes

---
+++ Canopy[BP]/scripts/src/classes/simplayer/PlayerInfoSaver.js
@@ -0,0 +1,94 @@
+import { world, system, DimensionTypes, TicksPerSecond, EntityComponentTypes } from "@minecraft/server";
+import { UnderstudyInventorySaver } from "./UnderstudyInventorySaver";
+import { noSimplayerSaving } from "../../rules/simplayer/noSimplayerSaving";
+import { UnderstudySaveInfoError } from "../errors/UnderstudySaveInfoError";
+import { UnderstudyNotConnectedError } from "../errors/UnderstudyNotConnectedError";
+
+export class PlayerInfoSaver {
+    saveInterval = 600;
+    #understudy;
+    #inventory;
+
+    constructor(understudy) {
+        this.#understudy = understudy;
+        this.#inventory = new UnderstudyInventorySaver(understudy);
+    }
+
+    onConnectedTick() {
+        this.#saveOnInterval();
+    }
+
+    #saveOnInterval() {
+        if (noSimplayerSaving.getValue())
+            return;
+        if ((system.currentTick - this.#understudy.createdTick) % this.saveInterval === 0) {
+            this.save();
+            return;
+        }
+        if (!this.#understudy.actions.isEmpty()) {
+            if ((system.currentTick - this.#understudy.createdTick) % (TicksPerSecond * 5) === 0)
+                this.save();
+            else
+                this.#inventory.saveWithoutNBT();
+        }
+    }
+
+    get() {
+        if (noSimplayerSaving.getValue())
+            throw new UnderstudySaveInfoError(`Player ${this.#understudy.name} has no player info saved due to '${noSimplayerSaving.getID()}' rule being enabled`);
+        let playerInfo;
+        try {
+            playerInfo = JSON.parse(world.getDynamicProperty(`${this.#understudy.name}:playerinfo`));
+        } catch (error) {
+            if (error.name === 'SyntaxError')
+                throw new UnderstudySaveInfoError(`Player ${this.#understudy.name} has corrupted player info saved, unable to parse player info.`);
+            throw error;
+        }
+        return playerInfo;
+    }
+
+    save() {
+        if (noSimplayerSaving.getValue())
+            return;
+        if (!this.#understudy.isConnected())
+            throw new UnderstudyNotConnectedError();
+        const simulatedPlayer = this.#understudy.simulatedPlayer;
+        const playerInfo = {
+            location: simulatedPlayer.location,
+            rotation: this.#understudy.headRotation,
+            dimensionId: simulatedPlayer.dimension.id,
+            gameMode: simulatedPlayer.getGameMode(),
+            projectileIds: this.#findOwnedProjectileIds()
+        };
+        world.setDynamicProperty(`${this.#understudy.name}:playerinfo`, JSON.stringify(playerInfo));
+        this.#inventory.save();
+    }
+
+    #findOwnedProjectileIds() {
+        let projectileIds = [];
+        for (const dimensionType of DimensionTypes.getAll()) {
+            const dimension = world.getDimension(dimensionType.typeId);
+            const projectiles = dimension.getEntities().filter(entity => {
+                const projectileComponent = entity.getComponent(EntityComponentTypes.Projectile);
+                return projectileComponent?.owner === this.#understudy.simulatedPlayer;
+            });
+            projectileIds = projectileIds.concat(projectiles.map(projectile => projectile.id));
+        }
+        return projectileIds;
+    }
+
+    loadInventoryAndProjectileOwnership() {
+        const playerInfo = this.get();
+        this.#claimProjectileIds(playerInfo.projectileIds);
+        this.#inventory.load();
+    }
+
+    #claimProjectileIds(projectileIds) {
+        projectileIds?.forEach(projectileId => {
+            const projectile = world.getEntity(projectileId);
+            const projectileComponent = projectile?.getComponent(EntityComponentTypes.Projectile);
+            if (projectileComponent)
+                projectileComponent.owner = this.#understudy.simulatedPlayer;
+        });
+    }
+}
---
+++ Canopy[BP]/scripts/src/classes/simplayer/UnderstudyInventorySaver.js
@@ -0,0 +1,110 @@
+import { EntityComponentTypes, EquipmentSlot, world } from "@minecraft/server";
+import SRCItemDatabase from "../../lib/SRCItemDatabase/ItemDatabase.js";
+
+export class UnderstudyInventorySaver {
+    constructor(understudy) {
+        this.understudy = understudy;
+        const tableName = `bot_${understudy.name.substr(0, 8)}`;
+        this.itemDatabase = new SRCItemDatabase(tableName);
+        this.inventoryDP = `${tableName}_inventory`;
+        this.equippableDP = `${tableName}_equippable`;
+        this.inventoryDBKey = 'inv';
+        this.equippableDBKey = 'equ';
+    }
+
+    save() {
+        this.#saveInventoryItems({ saveNBT: true });
+        this.#saveEquippableItems({ saveNBT: true });
+    }
+
+    saveWithoutNBT() {
+        this.#saveInventoryItems({ saveNBT: false });
+        this.#saveEquippableItems({ saveNBT: false });
+    }
+
+    load() {
+        this.#loadInventoryItems();
+        this.#loadEquippableItems();
+    }
+
+    #saveInventoryItems({ saveNBT = true } = {}) {
+        const inventoryItems = {};
+        const inventoryContainer = this.understudy.getInventory();
+        if (inventoryContainer !== void 0) {
+            for (let i = 0; i < inventoryContainer.size; i++) {
+                const itemStack = inventoryContainer.getItem(i);
+                inventoryItems[i] = itemStack ?? void 0;
+            }
+            this.#saveItemsWithoutNBT(this.inventoryDP, inventoryItems);
+            if (saveNBT)
+                this.#saveItemsWithNBT(this.inventoryDBKey, inventoryItems);
+        }
+    }
+
+    #saveEquippableItems({ saveNBT = true } = {}) {
+        const equippableItems = {};
+        const equippable = this.understudy.simulatedPlayer.getComponent(EntityComponentTypes.Equippable);
+        if (equippable !== void 0) {
+            for (const equipmentSlot in EquipmentSlot) {
+                const itemStack = equippable.getEquipment(equipmentSlot);
+                if (itemStack !== void 0)
+                    equippableItems[equipmentSlot] = itemStack;
+            }
+            this.#saveItemsWithoutNBT(this.equippableDP, equippableItems);
+            if (saveNBT)
+                this.#saveItemsWithNBT(this.equippableDBKey, equippableItems);
+        }
+    }
+
+    #saveItemsWithoutNBT(dynamicProperty, itemStacks) {
+        const items = {};
+        for (const [key, itemStack] of Object.entries(itemStacks)) {
+            if (itemStack)
+                items[key] = { typeId: itemStack.typeId, amount: itemStack.amount };
+        }
+        world.setDynamicProperty(dynamicProperty, JSON.stringify(items));
+    }
+
+    #saveItemsWithNBT(DBKey, itemStacks) {
+        const itemsWithNBT = Object.values(itemStacks).filter(item => item !== void 0);
+        this.itemDatabase.setItems(DBKey, itemsWithNBT);
+    }
+
+    #loadInventoryItems() {
+        const inventoryContainer = this.understudy.getInventory();
+        if (inventoryContainer === void 0)
+            return;
+        const itemsWithoutNBTStr = world.getDynamicProperty(this.inventoryDP);
+        if (itemsWithoutNBTStr === '{}' || itemsWithoutNBTStr === void 0)
+            return;
+        const itemsWithoutNBT = JSON.parse(itemsWithoutNBTStr);
+        const itemsWithNBT = this.itemDatabase.getItems(this.inventoryDBKey);
+        for (let i = 0; i < inventoryContainer.size; i++) {
+            const itemWithoutNBT = itemsWithoutNBT[i];
+            let itemStack = void 0;
+            if (itemWithoutNBT !== void 0) {
+                itemStack = itemsWithNBT.find(item => item.typeId === itemWithoutNBT.typeId && item.amount === itemWithoutNBT.amount);
+                itemsWithNBT.splice(itemsWithNBT.indexOf(itemStack), 1);
+            }
+            inventoryContainer.setItem(i, itemStack);
+        }
+    }
+
+    #loadEquippableItems() {
+        const equippable = this.understudy.simulatedPlayer.getComponent(EntityComponentTypes.Equippable);
+        if (equippable === void 0)
+            return;
+        const itemsWithoutNBTStr = world.getDynamicProperty(this.equippableDP);
+        if (itemsWithoutNBTStr === '{}' || itemsWithoutNBTStr === void 0)
+            return;
+        const itemsWithoutNBT = JSON.parse(itemsWithoutNBTStr);
+        const itemsWithNBT = this.itemDatabase.getItems(this.equippableDBKey);
+        for (const equipmentSlot in EquipmentSlot) {
+            const itemWithoutNBT = itemsWithoutNBT[equipmentSlot];
+            let itemStack = void 0;
+            if (itemWithoutNBT !== void 0)
+                itemStack = itemsWithNBT.find(item => item.typeId === itemWithoutNBT.typeId && item.amount === itemWithoutNBT.amount);
+            equippable.setEquipment(equipmentSlot, itemStack);
+        }
+    }
+}
