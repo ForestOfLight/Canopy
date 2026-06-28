31255b7dfc31ea60f59ea2413402443ae7c858e1
ForestOfLight <briancassriel@gmail.com>
2026-06-28 09:34:31 -0700

    feat: filter out sulfur cubes with blocks inside from creativeOneHitKill

--- Canopy[BP]/scripts/src/rules/creativeOneHitKill.js
+++ Canopy[BP]/scripts/src/rules/creativeOneHitKill.js
@@ -1,5 +1,5 @@
 import { BooleanRule, Rules } from "../../lib/canopy/Canopy";
-import { world, InputButton, ButtonState, GameMode } from "@minecraft/server";
+import { world, InputButton, ButtonState, GameMode, EntityComponentTypes } from "@minecraft/server";
 
 new BooleanRule({
     category: 'Rules',
@@ -9,18 +9,30 @@ new BooleanRule({
 });
 
 world.afterEvents.entityHitEntity.subscribe((event) => {
-    if (!Rules.getNativeValue('creativeOneHitKill') || event.damagingEntity?.typeId !== 'minecraft:player') return;
-    if (!event.hitEntity) return;
+    if (!Rules.getNativeValue('creativeOneHitKill') || event.damagingEntity?.typeId !== 'minecraft:player')
+        return;
+    if (!event.hitEntity)
+        return;
+    if (isSulfurCubeWithBlockInside(event.hitEntity))
+        return;
     const player = event.damagingEntity;
     if (player.getGameMode() === GameMode.Creative) {
         if (player.inputInfo.getButtonState(InputButton.Sneak) === ButtonState.Pressed) {
             player.dimension.getEntities({ location: event.hitEntity.location, maxDistance: 3 }).forEach(entity => {
-                if (['item', 'player', 'experience_orb'].includes(entity.typeId.replace('minecraft:', ''))) return;
+                if (['item', 'player', 'experience_orb'].includes(entity.typeId.replace('minecraft:', '')))
+                    return;
                 entity?.kill();
             });
         } else {
-            if (event.hitEntity?.typeId === 'minecraft:player') return;
+            if (event.hitEntity?.typeId === 'minecraft:player')
+                return;
             event.hitEntity?.kill();
         }
     }
 });
+
+function isSulfurCubeWithBlockInside(entity) {
+    const frictionComponent = entity.getComponent(EntityComponentTypes.FrictionModifier);
+    const ageableComponent = entity.getComponent(EntityComponentTypes.Ageable);
+    return frictionComponent?.value !== 1 && !ageableComponent;
+}
