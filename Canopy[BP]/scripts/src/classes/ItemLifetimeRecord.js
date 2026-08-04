4abb58d944520bcca4ec527785daa916178cf873
ForestOfLight <briancassriel@gmail.com>
2026-08-04 15:14:58 -0700

    feat: add item dropped spawn reason to lifetime tracking

--- Canopy[BP]/scripts/src/classes/ItemLifetimeRecord.js
+++ Canopy[BP]/scripts/src/classes/ItemLifetimeRecord.js
@@ -5,8 +5,8 @@ export class ItemLifetimeRecord extends EntityLifetimeRecord {
     entityType;
     localizationKey;
 
-    constructor(itemEntity, spawnReason) {
-        super(itemEntity, spawnReason);
+    constructor(itemEntity, spawnReason, spawnPriority = 0) {
+        super(itemEntity, spawnReason, spawnPriority);
         const itemStack = itemEntity.getComponent(EntityComponentTypes.Item).itemStack;
         this.entityType = itemEntity.typeId + '-' + itemStack.typeId;
         this.localizationKey = { rawtext: [{ translate: itemEntity.localizationKey }, { text: ' §7(§f' }, { translate: itemStack.localizationKey }, { text: '§7)§f' }, ] };
