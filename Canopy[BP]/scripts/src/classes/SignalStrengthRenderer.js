1cfdeff63f4e9e7828c2d5ba55afd70246c6b2c9
ForestOfLight <briancassriel@gmail.com>
2026-08-11 18:05:46 -0700

    feat: renderSignalStrength turns the visualization as you turn

--- Canopy[BP]/scripts/src/classes/SignalStrengthRenderer.js
+++ Canopy[BP]/scripts/src/classes/SignalStrengthRenderer.js
@@ -1,12 +1,14 @@
 import { system, TextPrimitive, world } from "@minecraft/server";
-import { Vector } from "../../lib/Vector";
 
 export class SignalStrengthRenderer {
     block;
     dimension;
     visibleToPlayer;
     textShape;
-    runner = void 0;
+    #textOffset = { x: -0.0125, y: -7.7 / 16, z: 0.0925 };
+    #lastCardinalYaw = void 0;
+    #lastTextYaw = void 0;
+    #runner = void 0;
 
     constructor(block, dimension, visibleToPlayer) {
         this.block = block;
@@ -23,13 +25,13 @@ export class SignalStrengthRenderer {
 
     startRender() {
         this.createTextShape();
-        this.runner = system.runInterval(this.onTick.bind(this));
+        this.#runner = system.runInterval(this.onTick.bind(this));
     }
 
     stopRender() {
-        if (this.runner !== void 0) {
-            system.clearRun(this.runner);
-            this.runner = void 0;
+        if (this.#runner !== void 0) {
+            system.clearRun(this.#runner);
+            this.#runner = void 0;
         }
         this.textShape?.remove();
         this.textShape = void 0;
@@ -40,7 +42,22 @@ export class SignalStrengthRenderer {
             this.stopRender();
             return;
         }
+        this.updateFacingRotation();
         this.updateRedstonePower();
+    }
+
+    updateFacingRotation() {
+        if (!this.textShape || !this.visibleToPlayer?.isValid)
+            return;
+
+        const { cardinalYaw, textYaw } = this.#getCurrentFacing();
+        if (this.#lastCardinalYaw === cardinalYaw && this.#lastTextYaw === textYaw)
+            return;
+
+        this.#lastCardinalYaw = cardinalYaw;
+        this.#lastTextYaw = textYaw;
+        this.textShape.rotation = { x: 90, y: textYaw, z: 0 };
+        this.textShape.setLocation(this.#getTextLocationForYaw(cardinalYaw));
     }
 
     updateRedstonePower() {
@@ -50,11 +67,12 @@ export class SignalStrengthRenderer {
     }
 
     createTextShape() {
-        const dimensionlocation = Vector.from(this.block.center()).add(new Vector(-0.0125, -7.7/16, 0.0925));
-        dimensionlocation.dimension = this.dimension;
-        this.textShape = new TextPrimitive(dimensionlocation, String(this.#getRedstonePower(this.block)));
+        const { cardinalYaw, textYaw } = this.#getCurrentFacing();
+        this.#lastCardinalYaw = cardinalYaw;
+        this.#lastTextYaw = textYaw;
+        this.textShape = new TextPrimitive(this.#getTextLocationForYaw(cardinalYaw), String(this.#getRedstonePower(this.block)));
         this.textShape.backgroundColorOverride = { red: 0, green: 0, blue: 0, alpha: 0 };
-        this.textShape.rotation = { x: 90, y: 0, z: 0 };
+        this.textShape.rotation = { x: 90, y: textYaw, z: 0 };
         this.textShape.useRotation = true;
         this.textShape.depthTest = true;
         this.textShape.backfaceVisible = false;
@@ -75,5 +93,39 @@ export class SignalStrengthRenderer {
             return '?';
         }
     }
+
+    #snapYawToCardinal(yaw) {
+        const normalizedYaw = ((yaw % 360) + 360) % 360;
+        return (Math.round(normalizedYaw / 90) * 90) % 360;
+    }
+
+    #getTextYawForFacing(cardinalYaw) {
+        if (cardinalYaw === 90 || cardinalYaw === 270)
+            return (cardinalYaw + 180) % 360;
+        return cardinalYaw;
+    }
+
+    #getCurrentFacing() {
+        const playerYaw = this.visibleToPlayer?.isValid
+            ? this.visibleToPlayer.getRotation().y
+            : 0;
+        const cardinalYaw = this.#snapYawToCardinal(playerYaw);
+        const textYaw = this.#getTextYawForFacing(cardinalYaw);
+        return { cardinalYaw, textYaw };
+    }
+
+    #getTextLocationForYaw(yaw) {
+        const center = this.block.center();
+        const radians = yaw * (Math.PI / 180);
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+        const offsetX = this.#textOffset.x * cos - this.#textOffset.z * sin;
+        const offsetZ = this.#textOffset.x * sin + this.#textOffset.z * cos;
+        return {
+            x: center.x + offsetX,
+            y: center.y + this.#textOffset.y,
+            z: center.z + offsetZ,
+            dimension: this.dimension,
+        };
     }
 }
